dffbb1017d966bf459a82fb7cb1a9be2db93e9b5
Melvin Mathew <36991926+melvingm101@users.noreply.github.com>
2025-11-22 20:16:05 +0400

    fix: permissions and adding in JSON file

--- scanner_files/webcheck.sh
+++ scanner_files/webcheck.sh
@@ -3,4 +3,4 @@ echo "Please enter the website you wish to scan: "
 read website_name
 
 # website_name would contain something like http://test-website:3000
-whatweb -v -a 3 $website_name
+whatweb -v --log-json=scan_report.json -a 3 $website_name
