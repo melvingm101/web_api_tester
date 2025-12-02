e6849d0b5b5fdc43a3e2586b3a9f88d5557ebebd
Melvin Mathew <36991926+melvingm101@users.noreply.github.com>
2025-12-02 11:28:01 +0400

    feat: adding rich for better output

---
+++ scanner_files/utils/output_print.py
@@ -0,0 +1,25 @@
+# This file is for formatting the text output
+from rich.console import Console, Text
+from rich.style import Style
+from rich.theme import Theme
+from time import sleep
+
+custom_theme = Theme({
+    "info": "cyan3",
+    "warning": "magenta",
+    "danger": "bold red",
+    "success": "bold green",
+    "repr.number": "bold green blink"
+})
+
+console=Console(theme=custom_theme)
+
+def print_success(message):
+    console.print(message, style="success")
+
+def print_error(message):
+    console.print(message, style="danger")
+
+def print_progress_bar(func, message):
+    with console.status(message) as status:
+        func()
